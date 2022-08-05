bf83ac9cdfe41a1d0304a249407a5c570ac9167f
Anton <antontitov94@gmail.com>
2022-08-05 19:41:14 +0300

    Messanger

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+
+export const MessegeComponent = () => {
+  const message = "New message";
+  return <MessegeElement message={message} />;
+};
+
+
+
+export const MessegeElement = ({ message }) => {
+  return (
+    <div className="Messanger">
+      <h1>MESSANGER</h1>
+      <div>{message}</div>
+    </div>
+  )
+}
