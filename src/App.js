14c0c632bffca43d90423cbc205f7d5632a1b1f6
Anton <antontitov94@gmail.com>
2022-08-07 11:28:34 +0300

    hometask2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-import React from "react";
+
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 
-
-export const MessegeComponent = () => {
-  const message = "New message";
-  return <MessegeElement message={message} />;
-};
+function App() {
+  const [value, setValue] = useState("");
+  const [messageList, setMessageList] = useState([]);
 
 
+  const addMessage = () => {
+    const newAuthor = "User"
+    const message = {
+      value: value,
+      author: newAuthor
+    }
+    setMessageList(oldMessages => [...oldMessages, message]);
+  }
 
-export const MessegeElement = ({ message }) => {
+  const botMessage = () => {
+    const newAuthor = "Bot"
+    const message = {
+      value: "Добрый день",
+      author: newAuthor
+    }
+    setMessageList(oldMessages => [...oldMessages, message]);
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      if (messageList.length !== 0 && messageList[messageList.length - 1].author !== "Bot") {
+        botMessage()
+      }
+    }, 1000);
+  }, [messageList]);
+
+  const changeMessage = (changedMessage) => {
+    const newMessageValue = changedMessage.target.value
+    setValue(newMessageValue)
+  }
+
   return (
-    <div className="Messanger">
-      <h1>MESSANGER</h1>
-      <div>{message}</div>
+    <div className="mainWrapper">
+      <h1>Messanger</h1>
+      <div className="messageList">
+        {messageList.map((message, i) => (
+          <div key={i}><div>{message.author}: {message.value}</div></div>
+        ))}
+      </div>
+
+      <div className="inputWrapper">
+        <input
+          className="input"
+          onChange={changeMessage} />
+        <button onClick={addMessage}>Отправить</button>
+      </div>
     </div>
-  )
+  );
 }
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { MessegeComponent } from "./App.js";
-
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <MessegeComponent />
+<App/>
   </React.StrictMode>
 );
 
