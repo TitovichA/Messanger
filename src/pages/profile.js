0472feebba2a591e9d18b9cdea022478eb963f2e
Anton <antontitov94@gmail.com>
2022-08-30 22:15:33 +0300

    hometask5 update

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,29 +1,11 @@
-import React, { useCallback } from 'react';
-import { useDispatch, useSelector } from "react-redux";
-import { profileNameAction } from "../store/profile/actions";
+import React from 'react';
+import { ProfileForm } from "../components/profile/profile-form.js";
 
 
 export const ProfilePage = () => {
 
-  const { showName, name } = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const setShowName = useCallback(() => {
-    dispatch(profileNameAction);
-}, [dispatch]);
-
   return (
-    <div>
-      <h4>Profile</h4>
-      <input
-        type="checkbox"
-        checked={showName}
-        value={showName}
-        onChange={setShowName}
-      />
-      <span>Show Name</span>
-      {showName && <div>{name}</div>}
-    </div>
+<ProfileForm/>
   );
 };
 
--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers } from "redux";
 import { profileReducer } from "./profile";
 
 export const store = createStore(
-    combineReducers({ profileReducer }),
+    combineReducers({ profile: profileReducer }),
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
     window.__REDUX_DEVTOOLS_EXTENSION__()
 );
